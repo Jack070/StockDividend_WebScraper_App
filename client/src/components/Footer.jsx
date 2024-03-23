64f51e8302ea73c56c2ee3af5d28d40726e67d9b
Jack070 <30437092+Jack070@users.noreply.github.com>
2024-03-23 09:46:21 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function Footer(){
     const currentYear = new Date().getFullYear();
     return(
-        <footer>
+        <footer style={{ position: "fixed", bottom: "0" }}>
             <p>Copyright ⓒ {currentYear}</p>
         </footer> 
     )
